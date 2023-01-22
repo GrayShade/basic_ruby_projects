5c32390897b9ff43fc6136045af9b34f89efa5df
Gray <silicongray@gmail.com>
2023-01-22 15:45:17 +0500

    Do small refactoring changes

--- ruby_stock_picker/ruby_stock_picker.rb
+++ ruby_stock_picker/ruby_stock_picker.rb
@@ -2,9 +2,9 @@ def stock_picker(prices)
   pair = Hash.new([])
   prices.each_with_index do |ele1, idx1|
     prices[idx1 + 1..].each_with_index do |ele2, idx2|
-      price = ele2 - ele1
-      ele2_orgnl_indx = idx2 + idx1 + 1 # for mentioning correct day below
-      pair[price] += [[idx1, ele2_orgnl_indx]] unless price.negative?
+      profit = ele2 - ele1
+      ele2_orignl_indx = idx2 + idx1 + 1 # for mentioning correct day below
+      pair[profit] += [[idx1, ele2_orignl_indx]] unless profit.negative?
     end
   end
   pair[pair.keys.max].each { |arr| p arr } # catering multiple pairs too
