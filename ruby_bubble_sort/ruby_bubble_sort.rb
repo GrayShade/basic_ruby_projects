1c5862e4f7bfcfddc2e958967fed75590d02c9b4
Gray <silicongray@gmail.com>
2023-01-23 11:45:09 +0500

    Refactor bubble_sort & test afterwards

--- ruby_bubble_sort/ruby_bubble_sort.rb
+++ ruby_bubble_sort/ruby_bubble_sort.rb
@@ -2,15 +2,15 @@ require 'pry-byebug'
 def bubble_sort(arr)
   flag = false
   while flag == false
-    counter = 0
+    flag = true
     arr[0..-2].each_with_index do |ele, idx|
       next if ele <= arr[idx + 1]
 
       # parallel assignment does not require using temp variable:
       arr[idx], arr[idx + 1] = arr[idx + 1], arr[idx]
-      counter = 1
+      # set flag to false if there was any iteration with swapping:
+      flag = false
     end
-    flag = true if counter.zero?
   end
   arr
 end
